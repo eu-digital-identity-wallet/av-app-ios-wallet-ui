aaac21c06b5bd1df362c333507ed4b22ca94e93b
Bharat Jagtap <bharat.jagtap@t-systems.com>
2025-05-22 15:13:21 +0530

    Adds the partially developed tab view in Welcome flow

--- Modules/feature-onboarding/Sources/UI/OnboardingHome/OnboardingHomeView.swift
+++ Modules/feature-onboarding/Sources/UI/OnboardingHome/OnboardingHomeView.swift
@@ -18,7 +18,15 @@ struct OnboardingHomeView<Router: RouterHost>: View {
     }
 
     var body: some View {
-        content(state: viewModel.viewState)
+        ContentScreenView(
+            padding: .zero,
+            canScroll: true,
+            errorConfig: viewModel.viewState.error,
+            background: Theme.shared.color.surface,
+            navigationTitle: .details
+        ) {
+            content(state: viewModel.viewState)
+        }
     }
 }
 
@@ -26,6 +34,40 @@ struct OnboardingHomeView<Router: RouterHost>: View {
 @ViewBuilder
 private func content(state: OnboardingHomeViewState) -> some View {
     VStack {
-        Text("Onboarding Home View")
+        AVSegment(items: ["Welcome", "Consent", "Security", "Verification"])
+        Spacer()
+    }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(Theme.shared.color.surface)
+}
+
+struct AVSegment: View {
+    var items: [String]
+    @State var selectedIndex: Int = 0
+
+    var body: some View {
+        HStack {
+            ForEach(items.indices, id: \.self) { index in
+                Text(items[index])
+                    .font(Theme.shared.font.labelMedium.font)
+                    .foregroundColor( selectedIndex == index ? Theme.shared.color.blue : Theme.shared.color.darkGrey)
+                    .padding(.all, 8)
+                    .onTapGesture {
+                        withAnimation {
+                            selectedIndex = index
+                        }
+                    }
+            }
+        }
+        .padding(.horizontal, 12)
+        .padding(.vertical, 4)
+        .background(Theme.shared.color.surface)
+        .shadow(color: .black.opacity(0.15), radius: 4, x: 0, y: 4)
+        .cornerRadius(10.0)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10.0)
+                .inset(by: 0.25)
+                .stroke(.black.opacity(0.15), lineWidth: 0.5)
+        )
     }
 }
