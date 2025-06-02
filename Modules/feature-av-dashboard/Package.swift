cacca83b9f500c3fe725cb93a7b06e41400d1c6a
Sachin Shinde <sachin.shinde@t-systems.com>
2025-06-02 14:46:00 +0530

    added package.swift

---
+++ Modules/feature-av-dashboard/Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "feature-av-dashboard",
+  platforms: [.iOS(.v16)],
+  products: [
+    .library(
+      name: "feature-av-dashboard",
+      targets: ["feature-av-dashboard"])
+  ],
+  dependencies: [
+    .package(name: "feature-common", path: "./feature-common"),
+    .package(name: "feature-test", path: "./feature-test")
+  ],
+  targets: [
+    .target(
+      name: "feature-av-dashboard",
+      dependencies: [
+        "feature-common"
+      ],
+      path: "./Sources"
+    ),
+    .testTarget(
+      name: "feature-av-dashboard-tests",
+      dependencies: [
+        "feature-av-dashboard",
+        "feature-common",
+        "feature-test"
+      ],
+      path: "./Tests")
+  ]
+)
